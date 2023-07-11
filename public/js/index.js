ef0877f75b41700fcd56a682536976fcfcfedbae
youzhen <yl301114@gmail.com>
2023-07-11 08:11:43 +0800

    scroll

--- public/js/index.js
+++ public/js/index.js
@@ -42,3 +42,11 @@ var swiperMember = new Swiper(".member_swiper", {
         }
     }
 })
+
+$(window).on("resize scroll",function(){
+    if($(window).scrollTop() > 0) {
+        $(".header").addClass("scroll")
+    }else {
+        $(".header").removeClass("scroll")
+    }
+})
