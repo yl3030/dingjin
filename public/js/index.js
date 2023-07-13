1129b01b5de3abe2b2ff3dd21317865b6604537a
youzhen <yl301114@gmail.com>
2023-07-13 09:12:07 +0800

    修改

--- public/js/index.js
+++ public/js/index.js
@@ -43,10 +43,21 @@ var swiperMember = new Swiper(".member_swiper", {
     }
 })
 
-$(window).on("resize scroll",function(){
-    if($(window).scrollTop() > 0) {
-        $(".header").addClass("scroll")
-    }else {
-        $(".header").removeClass("scroll")
-    }
+// $(window).on("resize scroll",function(){
+//     if($(window).scrollTop() > 0) {
+//         $(".header").addClass("scroll")
+//     }else {
+//         $(".header").removeClass("scroll")
+//     }
+// })
+
+$(".btn-download").click(function(){
+    $(".download-list").slideToggle(300);
 })
+
+$(document).click(function (event) {
+    var d_con = $(".btn-download");
+    if (!d_con.is(event.target) && d_con.has(event.target).length === 0) {
+        $(".download-list").slideUp(300);
+    }
+});
